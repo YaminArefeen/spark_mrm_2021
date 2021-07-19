3f604d75e3f516964eded9f671d803abf5ceaff7
YaminArefeen <yaminarefeen@gmail.com>
2021-07-19 13:09:45 -0400

    Delete figure9_wave_encoding_3d_slicegroup directory

--- figure9_wave_encoding_3d_slicegroup/matlabutils/begin.m
+++
@@ -1,5 +0,0 @@
-function [] = begin(text)
-%Function which does the beginning fprintf statement when I time so that I
-%dont have to type it as often
-fprintf(text); tic;
-end
--- figure9_wave_encoding_3d_slicegroup/matlabutils/rmse.m
+++
@@ -1,3 +0,0 @@
-function out = rmse(groundtruth,comparison)
-out = norm(groundtruth(:) - comparison(:))/norm(groundtruth(:));
-end
